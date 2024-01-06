59d87b6e74999a491512c7a8051ba8f7d37219a4
x3m4llk0 <YOUR_EMAIL>
2024-01-06 23:13:31 +0300

    all preparations before writing tests have been completed

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,1 +1,77 @@
+import asyncio
+import json
+from datetime import datetime
+
 import pytest
+from httpx import AsyncClient
+from sqlalchemy import insert
+
+from app.api.models.article import Article
+from app.api.models.user import User
+from app.core.config import settings
+from app.db.base import Base, async_session_maker, engine
+from app.main import app as fastapi_app
+
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_database():
+    # Обязательно убеждаемся, что работаем с тестовой БД
+    assert settings.MODE == "TEST"
+    async with engine.begin() as conn:
+        # Удаление всех заданных нами таблиц из БД
+        await conn.run_sync(Base.metadata.drop_all)
+        # Добавление всех заданных нами таблиц из БД
+        await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f"app/tests/mock_{model}.json", encoding="utf-8") as file:
+            return json.load(file)
+
+    users = open_mock_json("users")
+    articles = open_mock_json("articles")
+
+    # SQLAlchemy не принимает дату в текстовом формате, поэтому форматируем к datetime
+    for article in articles:
+        article["publication_date"] = datetime.strptime(article["publication_date"], "%Y-%m-%d")
+
+    async with async_session_maker() as session:
+        for Model, values in [
+            (User, users),
+            (Article, articles),
+        ]:
+            query = insert(Model).values(values)
+            await session.execute(query)
+
+        await session.commit()
+
+
+# Взято из документации к pytest-asyncio
+# Создаем новый event loop для прогона тестов
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="function")
+async def ac():
+    "Асинхронный клиент для тестирования эндпоинтов"
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="session")
+async def authenticated_ac():
+    "Асинхронный аутентифицированный клиент для тестирования эндпоинтов"
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        await ac.post("/api/v1/auth/login", json={
+            "email": "test@test.com",
+            "password": "test",
+        })
+        assert ac.cookies["booking_access_token"]
+        yield ac
+
+
