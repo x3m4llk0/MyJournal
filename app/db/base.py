9b36292bb0d5c8da48a506b5cb99556288d5044d
x3m4llk0 <YOUR_EMAIL>
2024-01-08 01:41:23 +0300

    add docker

--- app/db/base.py
+++ app/db/base.py
@@ -4,12 +4,8 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-if settings.MODE == "TEST":
-    DATABASE_URL = settings.TEST_DATABASE_URL
-    DATABASE_PARAMS = {"poolclass": NullPool}
-else:
-    DATABASE_URL = settings.DATABASE_URL
-    DATABASE_PARAMS = {}
+DATABASE_URL = settings.DATABASE_URL
+DATABASE_PARAMS = {}
 
 engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
